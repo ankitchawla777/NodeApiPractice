09aebaed58b36111868aaed5e32605f97ad1e4d0
Ankitchawla777 <ankitchawla777@gmail.com>
2018-02-21 13:24:39 +0530

    fix 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 mongoose.connect( "mongodb://localhost:27017/TodoApp").catch(e=>
 {
-    mongoose.connect( "mongodb://localhost:27017/TodoApp").catch(e=>
+    mongoose.connect( "mongodb://testuser:password777@ds227168.mlab.com:27168/testdb777").catch(e=>
     {
         console.log(JSON.stringify(e))
     })
