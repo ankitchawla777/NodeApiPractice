c474aaa53ead6cd543b18aa1673ba70a6b7e8bdf
Ankitchawla777 <ankitchawla777@gmail.com>
2018-02-21 13:02:31 +0530

    added localhost support

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect( "mongodb://testuser:password777@ds227168.mlab.com:27168/testdb777");
+mongoose.connect( "mongodb://localhost:27017/TodoApp" || "mongodb://testuser:password777@ds227168.mlab.com:27168/testdb777");
 
 module.exports={mongoose};
